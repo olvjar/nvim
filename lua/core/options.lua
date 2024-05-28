a5ebd6f1d78d2c88fb90d25e8a9530eeb3cc65c6
olvjar <43496985+olvjar@users.noreply.github.com>
2024-05-28 22:40:20 +0800

    feat: add local

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,31 +1,35 @@
 -- [[ Settings ]]
 
+local g = vim.g
+local opt = vim.opt
+
 -- General
-vim.opt.mouse = 'a' -- Enable mouse support
-vim.opt.clipboard = 'unnamedplus' -- Sync system clipboard
-vim.opt.undofile = true -- Save undo history
-vim.opt.have_nerd_font = true -- Have nerd font
+opt.mouse = 'a' -- Enable mouse support
+opt.clipboard = 'unnamedplus' -- Sync system clipboard
+opt.undofile = true -- Save undo history
+g.have_nerd_font = true -- Have nerd font
+
 
 -- UI
-vim.opt.termguicolors = true -- Enable 24-bit RGB colors
-vim.opt.number = true -- Show line numbers
-vim.opt.showmode = false -- Do not show mode
-vim.opt.breakindent = true -- Enable break indent
-vim.opt.cursorline = true -- Show which line cursor is on
+opt.termguicolors = true -- Enable 24-bit RGB colors
+opt.number = true -- Show line numbers
+opt.showmode = false -- Do not show mode
+opt.breakindent = true -- Enable break indent
+opt.cursorline = true -- Show which line cursor is on
 
-vim.opt.ignorecase = true -- Case insensitive
-vim.opt.smartcase = true -- Ignore lowercase for patterns
-vim.opt.signcolumn = 'yes' -- Keep signcolumn on default
+opt.ignorecase = true -- Case insensitive
+opt.smartcase = true -- Ignore lowercase for patterns
+opt.signcolumn = 'yes' -- Keep signcolumn on default
 
-vim.opt.splitright = true -- New vertical split right
-vim.opt.splitbelow = true -- New horizontal split below
+opt.splitright = true -- New vertical split right
+opt.splitbelow = true -- New horizontal split below
 
-vim.opt.list = true -- Whitelist characters
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.list = true -- Whitelist characters
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-vim.opt.inccommand = 'split' -- Preview live substitutions
-vim.opt.scrolloff = 10 -- Number of screen lines to keep above and below cursor
+opt.inccommand = 'split' -- Preview live substitutions
+opt.scrolloff = 10 -- Number of screen lines to keep above and below cursor
 
 -- Memory
-vim.opt.updatetime = 250 -- Decrease time update
-vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time
+opt.updatetime = 250 -- Decrease time update
+opt.timeoutlen = 300 -- Decrease mapped sequence wait time
