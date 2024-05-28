462f6a955f9eb51973a58bcee8d846cf4c33dba1
olvjar <43496985+olvjar@users.noreply.github.com>
2024-05-28 22:41:07 +0800

    feat: call colorscheme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,3 +1,9 @@
 vim.cmd 'colorscheme minischeme'
-require 'colorscheme'
+
+require('onedark').setup {
+  -- styles: dark, darker, cool, deep, warm, warmer, light
+  style = 'darker',
+  colors = { fg = '#b2bbcc' }, --default: #a0a8b7
+}
+require('onedark').load()
 
