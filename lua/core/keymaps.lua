13b9c57ff7f6c54ec8e52b60346f3b059a71a52c
olvjar <43496985+olvjar@users.noreply.github.com>
2024-05-28 22:40:47 +0800

    feat: add local

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,36 +1,40 @@
 --[[ Neovim keymaps ]]
 
+local g = vim.g
+local opt = vim.opt
+local keymap = vim.keymap.set
+
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+g.mapleader = ' '
+g.maplocalleader = ' '
 
 -- Set highlight on search
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+opt.hlsearch = true
+keymap('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Quick save
-vim.keymap.set('n', '<leader>s', ':w<CR>')
+keymap('n', '<leader>s', ':w<CR>')
 
 --[[ Plugin shortcuts ]]
 
 -- Neotree
-vim.keymap.set('n', '<leader>n', 'Neotree') -- open ntree
+keymap('n', '<leader>n', 'Neotree') -- open ntree
 
 -- Lazy.nvim
-vim.keymap.set('n', '<leader>l', '<cmd>:Lazy<cr>') -- open lazy.nvim
+keymap('n', '<leader>l', '<cmd>:Lazy<cr>') -- open lazy.nvim
