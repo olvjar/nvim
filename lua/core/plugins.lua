132e957cbfa009efd441a94b77d91f621e7098a5
olvjar <43496985+olvjar@users.noreply.github.com>
2024-05-28 22:40:33 +0800

    feat: add colorscheme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,5 +1,15 @@
 return {
     {
+  --[[ COLORSCHEME ]]
+       {
+        'navarasu/onedark.nvim',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+       },
+
+
+  --[[ PLUGINS ]]
+ 
         'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
         -- nerd font web icons
