16b059b2636a792203a1fc513b1bc30493dde5cf
olvjar <43496985+olvjar@users.noreply.github.com>
2024-11-22 20:34:31 +0800

    feat(plugins): add copilot

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,28 +1,32 @@
 return {
+  {
+    --[[ COLORSCHEME ]]
     {
-  --[[ COLORSCHEME ]]
-       {
-        'navarasu/onedark.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-       },
-
-
-  --[[ PLUGINS ]]
- 
-  	-- Detect tabstop and shiftwidth automatically
- 	{ 'tpope/vim-sleuth', opts = {} },
-
-        -- nerd font web icons
-        { 'nvim-tree/nvim-web-devicons', opts = {} },
-
-        -- "gc" to comment visual regions/lines
-        { 'numToStr/Comment.nvim', opts = {} },
-
-        -- Highlight todo, notes, etc in comments
-        { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-        -- import custom plugins
-        { import = 'plugins' },
+      'navarasu/onedark.nvim',
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
     },
+
+    --[[ PLUGINS ]]
+
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    
+    {
+      'github/copilot.vim',
+      cmd = 'Copilot',
+      event = 'InsertEnter',
+    },
+
+    -- nerd font web icons
+    { 'nvim-tree/nvim-web-devicons', opts = {} },
+
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    -- Highlight todo, notes, etc in comments
+    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+    -- import custom plugins
+    { import = 'plugins' },
+  },
 }
